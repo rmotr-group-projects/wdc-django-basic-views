b18cd61763558856829105f33083123193f86d92
Zona Gilreath <36095717+zonagilreath@users.noreply.github.com>
2019-01-15 11:44:32 -0600

    Update tests.py

--- django_basic_views/django_basic_views/tests.py
+++ django_basic_views/django_basic_views/tests.py
@@ -6,5 +6,5 @@ class TasksTestCase(TestCase):
     def test_task_1(self):
         """Should return Hello World while GETing to /hello-world URL"""
         response = self.client.get('/hello-world/')
-        assert response.status_code == 200
-        assert 'Hello World' in str(response.content)
+        assertEqual(response.status_code, 200)
+        assertIn('Hello World', str(response.content))
