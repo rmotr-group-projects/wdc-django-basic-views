68367ac4083e934b366561fe45eecaee0cf3d0f2
manofoar <joetynan@gmail.com>
2019-07-31 20:55:57 +0000

    whoops, needed to fix the path in urls.py to match that for the test

--- django_basic_views/django_basic_views/urls.py
+++ django_basic_views/django_basic_views/urls.py
@@ -19,7 +19,7 @@ from django_basic_views import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello_world/', views.hello_world, name = 'hello_world'),
+    path('hello-world/', views.hello_world, name = 'hello_world'),
     path('date/', views.current_date, name = 'current_date'),
     path('my_age/<int:year>/<int:month>/<int:day>', views.my_age, name = 'my_age'),
     path('next_birthday/<str:birthday>', views.next_birthday, name = 'next_birthday'),
